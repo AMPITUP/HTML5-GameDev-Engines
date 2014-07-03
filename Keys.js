22d58f625a66f3ddc2f03ce78aa7cea46ed38031
Alex <alexpeters@Alexs-MacBook-Pro.local>
2014-07-03 14:59:58 -0700

    create Keys.js

---
+++ Keys.js
@@ -0,0 +1,12 @@
+var currentKey = null;
+var keysDown = new Array(256);
+var virtKeys = false;
+
+A = 65; B = 66; C = 67; D = 68; E = 69; F = 70; G = 71;
+H = 72; I = 73; J = 74; K = 75; L = 76; M = 77; N = 78;
+O = 79; P = 80; Q = 81; R = 82; S = 83; T = 84; U = 85;
+V = 86; W = 87; X = 88; Y = 89; Z = 90;
+LEFT = 37; RIGHT = 39; UP = 38; DOWN = 40; SPACE = 32;
+ESC = 27; PGUP = 33; PGDOWN = 34; HOME = 36; END = 35;
+n0 = 48; n1 = 49; n2 = 50; n3 = 51; n4 = 52; n5 = 53;
+n6 = 54; n7 = 55; n8 = 56; n9 = 57; 
